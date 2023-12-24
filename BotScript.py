a088e78d63acab55315f25da4d00a514ad1b109c
Vetle Andreas <vetleat99@gmail.com>
2023-12-24 01:36:08 +0100

    Alpha v1.0 Development

--- BotScript.py
+++ BotScript.py
@@ -30,35 +30,36 @@ user_guesses = {}
 # Define a JSON file to store user submissions
 submits_file = 'submits.json'
 
-# Load user submissions from the JSON file, if it exists
-try:
-    with open(submits_file, 'r') as submits:
-        user_guesses = json.load(submits)
-except FileNotFoundError:
-    pass
-
 guildid = os.environ.get("guild-id")
 channelid = os.environ.get("channel-id")
 
 @bot.event
 async def on_ready():
+    global user_guesses
+    try:
+        with open('submits.json', 'r') as submits_file:
+            user_guesses = json.load(submits_file)
+    except FileNotFoundError:
+        user_guesses = {}
     print(f"Logged in as {bot.user}!")
+    print("Teams:", teams)
+    print("User guesses:", user_guesses)
     # Sync commands to a specific guild for testing
     # Replace 'YOUR_GUILD_ID' with your server's ID as an integer
     await tree.sync(guild=discord.Object(id=int(guildid)))
     channel = bot.get_channel(int(channelid))
     if channel:
         await channel.send("Kamikazetiden er kommet!")
-
-# Command to set the registration channel
-@tree.command()
+        
+@tree.command(name="settkamikazekanal", description="Setter kanalen Kamikazetips kommer i.", guild=discord.Object(int(guildid)))
 @commands.has_permissions(administrator=True)
-async def setregistrationchannel(ctx, channel: discord.TextChannel):
-    # Store the channel ID in a JSON file or a database
+async def setregistrationchannel(interaction: discord.Interaction, channel: discord.TextChannel):
     registration_channel_id = channel.id
     with open('registration_channel.json', 'w') as reg_file:
         json.dump({"channel_id": registration_channel_id}, reg_file)
-    await ctx.send(f"Registreringer vil nå bli sendt til {channel.mention}")
+
+    await interaction.response.send_message(f"Registreringer vil nå bli sendt til {channel.mention}")
+
     
 @tree.command(name="test", description="A simple test command", guild=discord.Object(int(guildid)))
 async def test(interaction: discord.Interaction):
@@ -66,7 +67,9 @@ async def test(interaction: discord.Interaction):
 
 @tree.command(name="kamikazetips", description="Registrer ditt kamikazetips", guild=discord.Object(int(guildid)))
 async def kamikazetips(interaction: discord.Interaction):
-    user_id = interaction.user.id
+    global user_guesses  # Ensure user_guesses is accessible
+
+    user_id = str(interaction.user.id)  # Convert user ID to string for consistency
     if user_id in user_guesses:
         await interaction.response.send_message("Du har allerede registrert ditt kamikazetips.")
         return
@@ -74,14 +77,15 @@ async def kamikazetips(interaction: discord.Interaction):
         await interaction.response.defer(ephemeral=True)
 
     temp_available_teams = available_teams.copy()
-    selected_teams = []
-
+    selected_team_ids = []  # Store team IDs
+    
     for i in range(1, 17):
         if not temp_available_teams:
             await interaction.followup.send("Ingen lag igjen å velge mellom.")
             break
 
-        options = [discord.SelectOption(label=team, value=team) for team in temp_available_teams]
+        options = [discord.SelectOption(label=team_name, value=str(team_id)) 
+                   for team_id, team_name in teams.items() if team_name in temp_available_teams]
 
         select_menu = discord.ui.Select(placeholder=f"Velg lag for {i}. plass:", options=options, custom_id=f"team_selection_{i}")
 
@@ -96,49 +100,49 @@ async def kamikazetips(interaction: discord.Interaction):
                     m.data.get('custom_id') == f"team_selection_{i}")
 
         try:
-            new_interaction = await bot.wait_for('interaction', check=check, timeout=120.0)  # 2 minutes timeout
+            new_interaction = await bot.wait_for('interaction', check=check, timeout=120.0)
         except asyncio.TimeoutError:
-            await interaction.followup.send("Du var superløk og brukte for lang tid. Prøv på nytt og tenk raskere >:( ", ephemeral=True)
+            await interaction.followup.send("Du var superløk og brukte for lang tid. Prøv på nytt og tenk raskere >:(", ephemeral=True)
             return
 
-        selected_team = new_interaction.data['values'][0]
-        selected_teams.append(selected_team)
-        temp_available_teams.remove(selected_team)
+        selected_team_id = int(new_interaction.data['values'][0])
+        selected_team_name = teams[selected_team_id]
+        selected_team_ids.append(selected_team_id)
+        temp_available_teams.remove(selected_team_name)
 
-        # Acknowledge the selection
-        await new_interaction.response.send_message(f"Du valgte {selected_team} for {i}.plass.", ephemeral=True)
-
-    user_guesses[user_id] = selected_teams
+    user_guesses[user_id] = selected_team_ids  # Update user_guesses with new registration
 
     # Save user submissions to the JSON file
     with open(submits_file, 'w') as submits:
         json.dump(user_guesses, submits)
+        await interaction.followup.send("Ditt kamikazetips er registrert.", ephemeral=True)
 
     # Retrieve the registration channel ID from JSON
     with open('registration_channel.json', 'r') as reg_file:
         data = json.load(reg_file)
         registration_channel_id = data.get("channel_id")
-
+    
     # Send the registration message to the designated channel
     registration_channel = bot.get_channel(registration_channel_id)
     if registration_channel:
         registration_message = f"{interaction.user.mention} har registrert sitt kamikazetips:\n"
-        for i, team in enumerate(selected_teams, start=1):
-            registration_message += f"{i}. {team}\n"
+        for i, team_id in enumerate(selected_team_ids, start=1):
+            team_name = teams.get(team_id, "Unknown Team")
+            registration_message += f"{i}. {team_name}\n"
         await registration_channel.send(registration_message)
     else:
-        await interaction.followup.send("Registration channel not set. Please use !setregistrationchannel to configure it.")
+        await interaction.followup.send("Registreringskanalen er ikke satt. Vennligst bruk !setregistrationchannel for å konfigurere den.")
 
-# Hybrid command to display user's guesses
 @tree.command(name="tipsetmitt", description="Se kamikazetipset ditt", guild=discord.Object(int(guildid)))
-async def tipsetmitt(ctx):
-    user_id = ctx.author.id
+async def tipsetmitt(interaction: discord.Interaction):
+    user_id = str(interaction.user.id)  # Ensure user_id is a string
     if user_id in user_guesses:
-        guesses = user_guesses[user_id]
-        formatted_guesses = [f"{i+1}. {teams[team_id]}" for i, team_id in enumerate(guesses)]
-        await ctx.send(f"{ctx.author.name}'s guesses:\n{', '.join(formatted_guesses)}")
+        team_ids = user_guesses[user_id]
+        formatted_guesses = [f"{i+1}. {teams.get(team_id, 'Unknown Team')}" for i, team_id in enumerate(team_ids)]
+        await interaction.response.send_message(f"{interaction.user.name}'s guesses:\n{', '.join(formatted_guesses)}")
     else:
-        await ctx.send("Du har ikke kamikazet inn noe tips enda.")
+        await interaction.response.send_message("Du har ikke kamikazet inn noe tips enda.")
+
         
 @tree.command(name='globalsync', description='Owner only')
 @commands.has_permissions(administrator=True)  # Restrict this command to administrators
@@ -152,15 +156,19 @@ async def localglobalsync(interaction: discord.Interaction):
     await bot.tree.sync()
     print('Command tree synced.')    
     
-@tree.command(name = "localsync", description = "Synkroniser commands lokalt i serveren", guild=discord.Object(int(guildid)))
-@commands.has_permissions(administrator=True)  # Restrict this command to administrators
-async def synccmd(ctx):
-    guild_id = int(guildid)  # Replace with your guild's ID
+@tree.command(name="localsync", description="Synkroniser commands lokalt i serveren", guild=discord.Object(int(guildid)))
+@commands.has_permissions(administrator=True)
+async def synccmd(interaction: discord.Interaction):
+    guild_id = int(guildid)
+    await interaction.response.defer()
+
     try:
-        synced_commands = await bot.tree.sync(guild=discord.Object(id=guild_id))
-        await ctx.send(f"Synced {len(synced_commands)} commands to the current server.")
+        synced_commands = await tree.sync(guild=discord.Object(id=guild_id))
+        await interaction.followup.send(f"Synced {len(synced_commands)} commands to the current server.")
     except Exception as e:
-        await ctx.send(f"An error occurred while syncing: {e}")
+        await interaction.followup.send(f"An error occurred while syncing: {e}")
+
+
 
 #@tree.command(name = "localsync", description = "local synkronisering", guild=discord.Object(int(guildid))) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 #@commands.has_permissions(administrator=True)  # Restrict this command to administrators
